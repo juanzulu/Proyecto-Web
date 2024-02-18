ea8451f7b0920d1d385cebcb035c4e35c2bbc6de
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-02-18 18:06:21 -0500

    changes

--- js/paginamain.js
+++ js/paginamain.js
@@ -1,3 +1,25 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("contenedor_de_la_imagen")[0].getElementsByTagName("img");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
+
+
+
+
 function cambiarImagen(elemento, idCaja) {
     var caja = document.getElementById(idCaja);
     var imagen = caja.querySelector('.imagen_caja');
