b33e5f2ad2dd317ba8fe83ad6924279cab9eeef8
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-03-07 15:21:15 -0500

    la barra de vet ya funcionando

--- src/main/resources/static/js/lista_usuario.js
+++ src/main/resources/static/js/lista_usuario.js
@@ -4,4 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Asegura que cada fila tenga la animación con un delay incremental
         row.style.animation = `fadeIn 0.5s ease-out ${0.5 + index * 0.1}s forwards`;
     });
+
+    
+   
 });
