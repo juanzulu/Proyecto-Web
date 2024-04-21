176a92a8a648b4cc0d6023cdb1196187020a1e3c
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-04-20 23:57:08 -0500

    veterinario controller

--- src/main/java/com/example/demo/controller/VeterinarioController.java
+++ src/main/java/com/example/demo/controller/VeterinarioController.java
@@ -4,19 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.example.demo.entity.Veterinario;
 import com.example.demo.repository.VeterinarioRepository;
 import com.example.demo.service.VeterinarioService;
 
 @RestController
-@RequestMapping("/Admin")
+@RequestMapping("/admin")
 @CrossOrigin (origins =  "http://localhost:4200")
 public class VeterinarioController {
 
@@ -31,14 +34,25 @@ public class VeterinarioController {
         return veterinarioService.SearchAll();
     }
 
-    @PostMapping("/agregar/veterinario")
+    @PostMapping("/agregar")
     public void add(@RequestBody Veterinario veterinario) {
         veterinarioService.add(veterinario);
+    }
+
+    @GetMapping("/veterinario/{id}")
+    public Veterinario SearchById(@PathVariable("id") Long id) {
+        return veterinarioService.SearchById(id);
     }
 
     @PutMapping ("/actualizar/veterinario")
     public void update(@RequestBody Veterinario veterinario) {
         veterinarioService.update(veterinario);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public void borrarVeterinario(@PathVariable("id") Long id) {
+        veterinarioService.deleletebyid(id);
     }
 
     
