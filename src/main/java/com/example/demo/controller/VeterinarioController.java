b442818b94ceaed62aceb795e1c9122fce2c2df3
Michael Gonzalez <michaeljoelgd@gmail.com>
2024-04-22 14:14:44 -0500

    Revert "Agregar 10 veterinarios quemados"
    This reverts commit aaee189c016797a1e86f6e9b3afffbd63fa297f7.

--- src/main/java/com/example/demo/controller/VeterinarioController.java
+++ src/main/java/com/example/demo/controller/VeterinarioController.java
@@ -4,22 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.example.demo.entity.Veterinario;
 import com.example.demo.repository.VeterinarioRepository;
 import com.example.demo.service.VeterinarioService;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/Admin")
 @CrossOrigin (origins =  "http://localhost:4200")
 public class VeterinarioController {
 
@@ -34,25 +31,14 @@ public class VeterinarioController {
         return veterinarioService.SearchAll();
     }
 
-    @PostMapping("/agregar")
-    public void agregarVeterinario(@RequestBody Veterinario veterinario) {
+    @PostMapping("/agregar/veterinario")
+    public void add(@RequestBody Veterinario veterinario) {
         veterinarioService.add(veterinario);
-    }
-
-    @GetMapping("/veterinario/{id}")
-    public Veterinario SearchById(@PathVariable("id") Long id) {
-        return veterinarioService.SearchById(id);
     }
 
     @PutMapping ("/actualizar/veterinario")
     public void update(@RequestBody Veterinario veterinario) {
         veterinarioService.update(veterinario);
-    }
-
-
-    @DeleteMapping("/delete/{id}")
-    public void borrarVeterinario(@PathVariable("id") Long id) {
-        veterinarioService.deleletebyid(id);
     }
 
     
