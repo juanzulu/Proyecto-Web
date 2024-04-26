d86bf3eadca608b3eaebc97b6f9b3a1d2ea7e2f8
Camilo Hernandez <133175453+Camilodez@users.noreply.github.com>
2024-04-26 15:24:11 -0500

    se borra todo lo del front html css y js

--- src/main/java/com/example/demo/entity/Droga.java
+++ src/main/java/com/example/demo/entity/Droga.java
@@ -3,7 +3,7 @@ package com.example.demo.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/example/demo/entity/Tratamiento.java
+++ src/main/java/com/example/demo/entity/Tratamiento.java
@@ -2,7 +2,7 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
